4afb77df8404f3d9444abf85147821b99f1d40a9
ahzia <ah.ziayosfi@gmail.com>
2021-11-16 21:40:22 +0430

    create Teacher class

---
+++ Teacher.rb
@@ -0,0 +1,10 @@
+require "./Person.rb"
+class Teacher < Person
+  def initialize(age, name = "Unknown", parent_permission = true,specialization)
+    super(age,name,parent_permission)
+    @specialization  = specialization
+  end
+  def can_use_services?
+    return true
+  end
+end
