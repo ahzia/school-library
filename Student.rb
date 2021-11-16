cf276f79b3a0ade841ed1a4d89d8ab271d0cc58c
ahzia <ah.ziayosfi@gmail.com>
2021-11-16 21:34:37 +0430

    create Student class

---
+++ Student.rb
@@ -0,0 +1,10 @@
+require "./Person.rb"
+class Student < Person
+  def initialize(age, name = "Unknown", parent_permission = true,classroom)
+    super(age,name,parent_permission)
+    @classroom = classroom
+  end
+  def play_hooky
+    return "¯\(ツ)/¯"
+  end
+end
