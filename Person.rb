5a6fc2f00de28c373cf1b89ab73d53818e9135ff
ahzia <ah.ziayosfi@gmail.com>
2021-11-17 01:14:00 +0430

    fix linters issue

--- Person.rb
+++ Person.rb
@@ -1,21 +1,20 @@
 class Person
   attr_accessor :name, :age, :id
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @age = age
     @name = name
     @parent_permission = parent_permission
   end
-  private
-  def is_of_age?
-    if @age >= 18
-      return true
-    else
-      return false
-    end
 
+  private
+
+  def is_of_age?
+    @age >= 18
   end
+
   def can_use_services?
-    return is_of_age? || @parent_permission
+    is_of_age? || @parent_permission
   end
 end
--- Student.rb
+++ Student.rb
@@ -6,6 +6,6 @@ class Student < Person
   end
 
   def play_hooky
-    return "¯\(ツ)/¯"
+    "¯\(ツ)/¯"
   end
 end
