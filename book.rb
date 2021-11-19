aa4e0194356737ecef6566949991b92a18e42362
ahzia <ah.ziayosfi@gmail.com>
2021-11-19 20:38:34 +0430

    create rent method in Book

--- book.rb
+++ book.rb
@@ -1,3 +1,5 @@
+require 'date'
+require './rental'
 class Book
   attr_accessor :title, :author
   attr_reader :rentals
@@ -6,5 +8,9 @@ class Book
     @title = title
     @author = author
     @rentals = []
+  end
+
+  def rent(person)
+    Rental.new(DateTime.now, person, self)
   end
 end
